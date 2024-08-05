335885f1a4ff74fa3e875a75745e4232dd8ffbbb
PinedaVictor <pinedavictor095@gmail.com>
2024-08-05 16:22:36 -0700

    Updated version number

--- vexal.rb
+++ vexal.rb
@@ -1,7 +1,7 @@
 class Vexal < Formula
     desc "AI CLI app built for assisting and automating developer workflow."
     homepage "https://www.vexal.io/"
-    url "https://github.com/PinedaVictor/vexal/releases/download/v1.0.3/vx"
+    url "https://github.com/PinedaVictor/vexal/releases/download/v1.1.4/vx"
     sha256 "114d1eab915cfa65785f219afc3067142e26d4cb70549185c5298f0ec6ddd1e8"
     license "BSD 3" 
   
