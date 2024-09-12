f5293688f52d93254befecceb53c8592d3d70774
PinedaVictor <pinedavictor095@gmail.com>
2024-09-12 11:40:12 -0700

    Updated new vx version and sha256

--- vexal.rb
+++ vexal.rb
@@ -1,8 +1,8 @@
 class Vexal < Formula
     desc "AI CLI app built for assisting and automating developer workflow."
     homepage "https://www.vexal.io/"
-    url "https://github.com/PinedaVictor/vexal/releases/download/v1.2.4/vx.tar.gz"
-    sha256 "e910e3986e5e3b2a4656b03cab355319033440a1e3e6baadbbc8515d142f2454"
+    url "https://github.com/PinedaVictor/vexal/releases/download/v1.2.5/vx.tar.gz"
+    sha256 "4a4921dcffda74849a086fb71606e9d452cafec0f2bd9d47547c840d35596f65"
     license "BSD 3" 
   
     def installgst
